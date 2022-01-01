d0f983f298c2314e3fbee3ab56ecec352c6ffb56
baixiao <bzh2lf163@163.com>
2022-01-01 11:41:41 +0800

    feat: finished-search

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -5,10 +5,20 @@
       <uni-search-bar @input="input" radius="100" cancelButton="none" :focus="true"></uni-search-bar>
    </view>
    <!-- 搜索建议列表-->
-   <view class="sugg-list">
+   <view class="sugg-list" v-if="searchResults.length !== 0">
      <view class="sugg-item" v-for="(item,i) in searchResults" :key="i" @click="goToDetail(item)">
        <view class="goods-name">{{item.goods_name}}</view>
        <uni-icons type="arrowright" size="16"></uni-icons>
+     </view>
+   </view>
+   <!-- 搜索历史区域-->
+   <view class="history-box" v-else>
+     <view class="history-title">
+       <text>搜索历史</text>
+       <uni-icons type="trash" size="17" @click="cleanHistory"></uni-icons>
+     </view>
+     <view class="history-list">
+       <uni-tag :text="item" v-for="(item,i) in histories" :key="i" @click="goToGoodsList(item)"></uni-tag>
      </view>
    </view>
   </view>
@@ -21,6 +31,7 @@
         timer:null,
         keyWord:'',
         searchResults:[],
+        historyList:[],
       };
     },
     methods:{
@@ -44,12 +55,38 @@
         const {data: res} = await uni.$http.get(`/api/public/v1/goods/qsearch?query=${this.keyWord}`);
         if(res.meta.status !== 200) return uni.$showMsg();
         this.searchResults = res.message
+        this.saveSearchHistory()
       },
       goToDetail(item) {
         uni.navigateTo({
           url:`/subpkg/goods_detail/goods_detail?goods_id=${item.goods_id}`
         })
       },
+      saveSearchHistory() {
+        /* this.historyList.push(this.keyWord) */
+        const set = new Set(this.historyList);
+        set.delete(this.keyWord);
+        set.add(this.keyWord);
+        this.historyList = Array.from(set);
+        uni.setStorageSync('kw',JSON.stringify(this.historyList))
+      },
+      cleanHistory() {
+        this.historyList = [];
+        uni.setStorageSync('kw','[]')
+      },
+      goToGoodsList(kw) {
+        uni.navigateTo({
+          url:`/subpkg/goods_list/goods_list?query=${kw}`
+        })
+      }
+    },
+    onLoad() {
+      this.historyList = JSON.parse(uni.getStorageSync('kw') || '[]')
+    },
+    computed:{
+      histories() {
+        return [...this.historyList].reverse()
+      }
     },
   }
 </script>
@@ -74,6 +111,29 @@
       overflow:hidden;
       text-overflow:ellipsis;
       margin-right: 3px;
+    }
+  }
+}
+.history-box {
+  padding: 0 5px;
+  .history-title {
+    height: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 13px;
+    border-bottom: 1px solid #efefef;
+    margin-bottom: 5px;
+  }
+  .history-list {
+    display: flex;
+    flex-wrap: wrap;
+    .uni-tag {
+      background-color: #f7f7f7;
+      border-color: #f7f7f7;
+      color: #333;
+      margin-top: 5px;
+      margin-right: 5px;
     }
   }
 }
